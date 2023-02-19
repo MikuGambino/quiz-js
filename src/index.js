8ddbdaba25c876f59ceea0b5b6e9c51f5ea0fda4
MikeGambino <dan.k-2011@yandex.ru>
2023-02-19 19:17:43 +0400

    add showing results function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {QuizData} from "./quiz_data.js";
 import Quiz from "./quiz.js";
+import { ResultData } from "./result_data.js";
 
-let quiz = new Quiz(QuizData);
+let quiz = new Quiz(QuizData, ResultData);
 quiz.nextQuestion();
--- src/quiz.js
+++ src/quiz.js
@@ -1,11 +1,12 @@
 export default class Quiz{
     
-    constructor(questions){
+    constructor(questions, results){
         this.answerElements = [
             document.querySelector('[value="0"]'),
             document.querySelector('[value="1"]'),
         ];
-        this.results = new Map();
+        this.results = results;
+        this.scores = new Map();
         this.questions = questions;
         this.step = 0;
         this.questionElement = document.querySelector("#question");
@@ -14,13 +15,15 @@ export default class Quiz{
     }
 
     nextQuestion(){
-        console.log("next");
+        console.log(this.step);
         if(this.step == this.questions.length){
-            // todo - final
+            this.showResults("dizn");
+            // todo - calculate
         }
         this.questionElement.innerHTML = this.questions[this.step]["question"];
         this.answerElements.forEach((el, index) => {
-            el.innerHTML = `<input required type=\"radio\" name=\"radio\">${this.questions[this.step]["answers"][index].text}`});
+            el.innerHTML = `<input required type=\"radio\" name=\"radio\">${this.questions[this.step]["answers"][index].text}`
+        });            
     }
 
     checkAnswer(){
@@ -29,8 +32,22 @@ export default class Quiz{
         else if(this.answerElements[1].control.checked) answer = this.questions[this.step]["answers"][1];
         if(answer == undefined) return;
         answer.effects.forEach((value, key) => {
-            if(!this.results.has(key)) this.results.set(key, 0);
-            this.results.set(key, this.results.get(key) + value);
+            if(!this.scores.has(key)) this.scores.set(key, 0);
+            this.scores.set(key, this.scores.get(key) + value);
         });
+        this.step++;
+        this.nextQuestion();
+    }
+
+    showResults(res){
+        document.querySelector(".radioContainer").remove();
+        document.querySelector("#submit").remove();
+        let resImg = document.createElement("img");
+        resImg.src = this.results[res].img;
+        let description = document.createElement("p");
+        description.innerHTML = this.results[res].text;
+        this.questionElement.innerHTML = this.results[res].heading;
+        this.questionElement.after(description);
+        description.after(resImg);
     }
 }
